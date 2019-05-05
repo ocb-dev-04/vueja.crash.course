2e491c98cca1685bd745305cc842e537fc303e46
ocb-dev-04 <42700726+ocb-dev-04@users.noreply.github.com>
2019-05-05 17:43:24 -0400

    *Add some components and some codes

---
+++ test/src/components/Todos.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo" />
+        </div>
+    </div>
+</template>
+
+<script>
+
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: "Todos",
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+h1 {
+    color: red;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: bold;
+}
+
+</style>
